5a2d97c89573a34c1b794e962d52c79220b3bf2e
Muhammad <jamiludeensani@gmail.com>
2024-05-26 00:52:59 +0300

    sub-7

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
 
   const heroImageBackgroundWrapper: CSSProperties = {
-    backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0)), url("/assets/view-1.jpeg")`,
+    backgroundImage: `linear-gradient(rgba(0,0,0,0.7), rgba(0,0,0,0.0)), url("/assets/view-2.jpeg")`,
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
   };
